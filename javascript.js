1879fcfc720cafe795bc2d4e38fbd39fad2e10a4
Stalloyde <stalloyde93@gmail.com>
2022-06-12 23:48:24 +0800

    Add hovering sketch upon mouse click

--- javascript.js
+++ javascript.js
@@ -1,23 +1,23 @@
-for (i = 1; i <= 256; i ++) {
+function createGrid () {
+    for (i = 1; i <= 256; i ++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-item");
         newDiv.textContent = "";
         document.querySelector(".grid").appendChild(newDiv);
     }
+}
 
-function sketchBlue () {
-
+function togglePen () {
     const gridItem = document.querySelectorAll(".grid-item");
-    gridItem.forEach(item => item.addEventListener("mousedown", startInk));
-    }
+    gridItem.forEach(item => item.addEventListener("mouse", hover));
 
-    function startInk () {
-        const gridItem = document.querySelectorAll(".grid-item");
-        gridItem.forEach(item => item.classList.toggle("blue-ink"));
-    }
-
+    function hover () {
+    gridItem.forEach(item => item.addEventListener("mouseover", function ()  {
+    this.classList.add("sketch-blue")}));
+  }
+}
     
-startInk();   
+createGrid ();
+togglePen();
+//right idea to mousedown = hover, mouseup = stopHover. how to write function for stopHover?
 
-///entire grid turns blue as all grids are classed as .grid-item. how do i individualise them ? 
-    // use array loops to give each grid an index?
