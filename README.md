ffa03160aed3651fb9932a55a16bd3f6ae58b462
Stalloyde <stalloyde93@gmail.com>
2022-06-20 23:29:38 +0800

    WIP: Complete black and random colour button Clean up JS code Add usage instruction

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 1) Learnt CSS Grid
-2) Learnt a few new CSS & HTML tricks:
+2) Learnt a few new CSS,HTML,JS tricks:
     - .oninput
     - .this
     - Background-image: linear gradient;
-3) How to remove all node children using a loop.
-3) How to retrieve value of a slider and use said value to implement a dynamic grid size modifier
+3) How to remove all node children using a loop
+4) How to retrieve value of a slider and use said value to implement a dynamic grid size modifier
+5) How to get random color
+6) Deeper understanding of querySelector, querySelectorAll, addEventListener.
--- javascript.js
+++ javascript.js
@@ -1,48 +1,52 @@
 const slider = document.getElementById("slider");
 const output = document.getElementById("output");
 const grid = document.querySelector(".grid-container");
+const gridItem = document.querySelectorAll(".grid-item");
+const blackBtn = document.getElementById("black");
+const randomBtn = document.getElementById("random");
+const rgbBtn = document.getElementById("rgb");
+const eraserBtn = document.getElementById("eraser");
+const clearBtn = document.getElementById("clear");
 
 slider.oninput = function getOutputValue () {
     output.innerHTML = `${this.value} x ${this.value}`;
     }
 
-function createGrid () {
-    for (i = 1; i <= 256; i++) {
-        const div = document.createElement("div")
+for (i = 1; i <= 256; i++) {
+    const div = document.createElement("div")
+    div.classList.add("grid-item");
+    grid.appendChild(div);
+}      
+
+gridItem.forEach(item => item.addEventListener("mouseover", function () {
+    this.style.backgroundColor = "black";
+}))
+
+blackBtn.addEventListener("click", function () {
+    const gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(item => item.addEventListener("mouseover", function () {             
+        this.style.backgroundColor = "black";
+    }));
+})
+
+randomBtn.addEventListener("click", function () {
+    const gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(item => item.addEventListener("mouseover", function () {   
+        this.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    }));
+})
+
+slider.addEventListener("input", function () {
+    const calGridSize = this.value * this.value;
+    
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+    for (i = 1; i <= calGridSize; i++) {
+        const sqRoot = Math.sqrt(calGridSize);
+        const div = document.createElement("div");
         div.classList.add("grid-item");
         grid.appendChild(div);
+        grid.style.setProperty("grid-template-columns", "repeat(" + sqRoot + ", 1fr)");
     }
-}
-
-function modifyGrid () {
-    slider.addEventListener("input", function () {
-        const calGridSize = this.value * this.value;
-        
-        while (grid.firstChild) {
-            grid.removeChild(grid.lastChild);
-        }
-
-        for (i = 1; i <= calGridSize; i++) {
-            const sqRoot = Math.sqrt(calGridSize);
-            const div = document.createElement("div");
-            div.classList.add("grid-item");
-            grid.appendChild(div);
-            grid.style.setProperty("grid-template-columns", "repeat(" + sqRoot + ", 1fr)");
-        }
-    togglePen();
-    })       
-}
-
-function togglePen () {
-    const gridItem = document.querySelectorAll(".grid-item");
-    gridItem.forEach(item => item.addEventListener("mousedown", hover));
-
-    function hover () {
-    gridItem.forEach(item => item.addEventListener("mouseover", function ()  {
-    this.classList.add("sketch-blue")}));
-  }
-}
-
-createGrid();
-togglePen();
-modifyGrid();
+}) 
